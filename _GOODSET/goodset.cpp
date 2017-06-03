fef107f1d62f25f572a279421d4f5f2647bc0268
nipil <nipil@users.noreply.github.com>
2017-06-03 12:05:53 +0200

    passed: respected min/max value constraint
    max value is 500
    test is a sum
    max set size 100
    500+ any > 500, so can't be present
    min value when set full 400
    ... simply start at 500 and descend !

--- _GOODSET/goodset.cpp
+++ _GOODSET/goodset.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
- 
+
 #ifdef DEBUGBUILD
     #define D(x) do { std::cerr << x; } while (0)
     #undef NDEBUG
@@ -8,15 +8,15 @@
     #define D(x)
     #define NDEBUG
 #endif
- 
+
 #include <cassert>
 #include <cmath>
- 
+
 using namespace std;
- 
+
 const int MIN_AMOUNT = 1;
 const int MAX_AMOUNT = 100;
- 
+
 template <typename T>
 ostream &operator << (ostream &a, const vector<T> &b)
 {
@@ -26,37 +26,21 @@ ostream &operator << (ostream &a, const vector<T> &b)
     }
 	return a;
 }
- 
+
 void solve(int n) {
- 
+
     D("n:" << n << endl);
- 
+
     vector<int> goodset;
-    
-    int a=1, b=2, c;
-    
-    if (n == 1) {
-        cout << a << endl;
-        return;
-    }
- 
-    cout << a << " " << b;
-    if (n == 2) {
-            cout << endl;
-            return;
-    }
-    
-    n -= 2;
+
+    int a=500;
     while(n--) {
-        c = b + a + 1;
-        cout << " " << c;
-        a = b;
-        b = c;
+        cout << a-- << " ";
     }
-    
+
     cout << endl;
 }
- 
+
 int main(int argc, char **argv)
 {
     int T;
@@ -64,18 +48,18 @@ int main(int argc, char **argv)
     D("T=" << T << endl);
     assert(T >= MIN_AMOUNT);
     assert(T <= MAX_AMOUNT);
- 
+
     for (int i=0; i<T; i++) {
         D("test: " << i + 1 << "/" << T << endl);
-        
+
         int n;
         cin >> n;
         D("n=" << n << endl);
         assert(n >= MIN_AMOUNT);
         assert(n <= MAX_AMOUNT);
- 
+
         solve(n);
     }
- 
+
 	return 0;
 }
